0ff5925ed3cb8748fd69e026e6745fb1e16bed3b
David Ackerman <ackermand@janelia.hhmi.org>
2025-02-24 13:27:03 -0500

    add code and python files for running mutex watershed on whole datasets

---
+++ whole_datasets/mws/2025-02-15_3mb.py
@@ -0,0 +1,80 @@
+import rusty_mws
+import datetime
+from pathlib import Path
+import rusty_mws
+import logging
+from annotation_processing_utils.utils.parse_data_path import parse_data_path
+from datetime import datetime
+
+logger: logging.Logger = logging.getLogger(name=__name__)
+import getpass
+
+username = getpass.getuser()
+dataset = "jrc_22ak351-leaf-3mb"
+mask_path = f"/nrs/cellmap/ackermand/cellmap/leaf-gall/prediction_masks.zarr/dilation_iterations_9_{dataset}/s0"
+
+leaf_part = dataset.split("-")[-1]
+base_path = (
+    f"/nrs/cellmap/ackermand/predictions/cellmap_experiments/{dataset}/{dataset}.zarr"
+)
+affinities_path = (
+    f"{base_path}/predictions/2025-02-15_{leaf_part}/plasmodesmata_affs_lsds/0__affs"
+)
+segmentation_path = (
+    f"{base_path}/processed/2025-02-15_{leaf_part}/plasmodesmata_affs_lsds/0__affs"
+)
+
+affinities_file, affinities_dataset = parse_data_path(affinities_path)
+output_file, output_dataset = parse_data_path(segmentation_path)
+if mask_path is not None:
+    mask_file, mask_dataset = parse_data_path(mask_path)
+else:
+    mask_file = None
+    mask_dataset = None
+adj_bias = 0.5
+lr_bias = -1.2
+filter_val = 0.5
+lr_bias_ratio = -0.08
+timestamp = datetime.now().strftime(f"%Y%m%d/%H%M%S/")
+log_dir = f"/nrs/cellmap/{username}/logs/daisy_logs/mws_logs/{affinities_dataset}_filter_val_{filter_val}_lrb_ratio_{lr_bias_ratio}_adj_{adj_bias}_lr_{lr_bias}/{timestamp}"
+pp: rusty_mws.PostProcessor = rusty_mws.PostProcessor(
+    affs_file=affinities_file,
+    affs_dataset=affinities_dataset,
+    fragments_file=output_file,
+    fragments_dataset=f"{output_dataset}_filter_val_{filter_val}_lrb_ratio_{lr_bias_ratio}_adj_{adj_bias}_lr_{lr_bias}_frags",
+    seg_file=output_file,
+    seg_dataset=f"{output_dataset}_filter_val_{filter_val}_lrb_ratio_{lr_bias_ratio}_adj_{adj_bias}_lr_{lr_bias}_segs",
+    db_host="mongodb://cellmapAdmin:LUwWXkSY8N3AqCcw@cellmap-mongo.int.janelia.org:27017",
+    db_name=f"cellmap_postprocessing_{username}",
+    use_mongo=True,
+    lr_bias_ratio=lr_bias_ratio,
+    adj_bias=adj_bias,
+    lr_bias=lr_bias,
+    mask_file=mask_file,
+    mask_dataset=mask_dataset,
+    nworkers_frags=63,
+    nworkers_lut=63,
+    nworkers_supervox=63,
+    filter_val=filter_val,
+    neighborhood=[
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+        [3, 0, 0],
+        [0, 3, 0],
+        [0, 0, 3],
+        [9, 0, 0],
+        [0, 9, 0],
+        [0, 0, 9],
+    ],
+    log_dir=log_dir,
+)
+success = pp.run_pred_segmentation_pipeline()
+if success:
+    print(
+        f"completed ({affinities_dataset}_filter_val_{filter_val}_lrb_ratio_{lr_bias_ratio}_adj_{adj_bias}_lr_{lr_bias}) all tasks successfully!"
+    )
+else:
+    print(
+        f"failed ({affinities_dataset}_filter_val_{filter_val}_lrb_ratio_{lr_bias_ratio}_adj_{adj_bias}_lr_{lr_bias}) with status {success} Some task failed."
+    )
